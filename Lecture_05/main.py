c8da6d1694790bbd1fb57a9eacb268383cd091e7
Mokarbeen Ansari <52609206+MokarbeenAnsari@users.noreply.github.com>
2025-04-18 14:46:11 +0530

    Add files via upload

---
+++ Lecture_05/main.py
@@ -0,0 +1,7 @@
+a = 'This is string'
+print(a)
+
+'''
+This is line 1
+This is line 2
+'''
---
+++ Lecture_19/my_module.py
@@ -0,0 +1,5 @@
+def hello():
+    print("Hello from my_module.py")
+
+def bye():
+    print("Bye from my_module.py")
